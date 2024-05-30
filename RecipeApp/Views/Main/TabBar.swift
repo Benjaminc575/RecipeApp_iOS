aa5c183bcb108cd959fbd3499dd165d88a299c5d
Benjamin Cardenas <benjaminc5751@gmail.com>
2024-05-30 17:59:00 -0500

    Set up Initial Tab View

---
+++ RecipeApp/Views/Main/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  RecipeApp
+//
+//  Created by Benjamin Cardenas on 5/30/24.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
