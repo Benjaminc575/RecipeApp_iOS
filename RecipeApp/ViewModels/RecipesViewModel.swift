c967d813f51f903548e9dfd1e09781096a4112af
Benjamin Cardenas <benjaminc5751@gmail.com>
2024-06-05 13:10:23 -0500

    Added Ability to Custom add recipes.

--- RecipeApp/ViewModels/RecipesViewModel.swift
+++ RecipeApp/ViewModels/RecipesViewModel.swift
@@ -13,4 +13,9 @@ class RecipesViewModel: ObservableObject {
     init() {
         recipes = Recipe.all 
     }
+    
+    
+    func addRecipe(recipe: Recipe) {
+        recipes.append(recipe)
+    }
 }
